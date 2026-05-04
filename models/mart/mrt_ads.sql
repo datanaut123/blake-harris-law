b7b0db841360e179ff83fa46f5635a79d5d83e17
Naman_Kashyap <data@dataspoton.com>
2026-05-05 00:12:29 +0530

    Data_Model

--- models/mart/mrt_ads.sql
+++ models/mart/mrt_ads.sql
@@ -27,3 +27,19 @@ select
     'Paid' as channel
 
     from {{ref('stg_ba_campaigns')}}
+
+        union all 
+
+select 
+    campaign_id,
+    campaign_name,
+    date,
+    clicks,
+    status,
+    conversions,
+    spend,
+    impressions,
+    'Meta' as platform,
+    'Paid' as channel
+
+    from {{ref('stg_ma_ad_insights')}}
